d0ce50d23975439687c61b87557e7d8c1abb8f3b
Grzesiek <g.durtan@temida.io>
2023-03-03 20:26:25 +0100

    add types

---
+++ types.ts
@@ -0,0 +1,31 @@
+export type Dimentions = "none" | "xs" | "sm" | "md" | "lg" | "xl" | "2xl";
+export type TextSize =
+  | "xs"
+  | "sm"
+  | "base"
+  | "md"
+  | "lg"
+  | "xl"
+  | "2xl"
+  | "3xl";
+export type TextWeight = "normal" | "bold" | "upbold";
+export type Color =
+  | "primary"
+  | "primary-light"
+  | "secondary"
+  | "secondary-light"
+  | "tertiary"
+  | "tertiary-light"
+  | "error"
+  | "error-light"
+  | "success"
+  | "success-light"
+  | "warning"
+  | "warning-light"
+  | "info"
+  | "info-light"
+  | "gray"
+  | "gray-light"
+  | "white";
+export type Rounded = "md" | "lg" | "2xl" | "full";
+export type Shadow = "md" | "lg" | "full";
